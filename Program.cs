6ced0bf532d0bbde271c7d63dd039ef75d9b7a6e
Vadim <bbrb2110@gmail.com>
2021-08-22 22:45:47 +0300

    Clear project

--- Program.cs
+++ Program.cs
@@ -6,22 +6,7 @@ namespace ConsoleDBApp
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("a");
-            int a = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("b");
-            int b = Int32.Parse(Console.ReadLine());
-
-            int Sum = a + b;
-            Console.WriteLine(Sum);
-
-
-            // WTF222222222
-
-            Console.WriteLine("Test21");
-
-            Console.WriteLine("Test22");
-            //
+                        
         }
     }
 }
