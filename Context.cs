d9e1997bd8d0d55762dcb9e41c4cd67c148f8b0f
bbrb@ukr.net <bbrb@ukr.net>
2021-08-25 10:59:09 +0300

    Add Context.cs and App.config

---
+++ Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using System;
+
+namespace ConsoleDBApp
+{
+    class Context : DbContext
+    {
+        protected Context() : base("DbConnectionString")
+        {
+
+        }
+    }
+}
